4038c2525527aa70ae9a4e2042d1b502bc966621
manoloSKa <47843164+manoloSKa@users.noreply.github.com>
2023-06-08 23:26:41 -0600

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+    /*Declaracion de variables*/
+    private String palo;
+    private String color;
+    private String valor;
+
+    /*Instanciación*/
+    public Card (String palo, String color, String valor){
+        this.palo = palo;
+        this.color = color;
+        this.valor = valor;
+    }
+    /*Metodos get*/
+    public String getPalo(){
+        return palo;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public String getValor(){
+        return valor;
+    }
+
+    /*Anotación para sobreeescribir metodos*/
+    @Override
+    public String toString(){
+        return palo+","+color+","+valor;
+    }
 }
